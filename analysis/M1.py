0042aee600d4fc347f2ed09c0d704e3c649ea925
hhucyl <hhucyl@hotmail.com>
2020-02-22 14:04:44 +0800

    post

--- analysis/M1.py
+++ analysis/M1.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 prefix = "/media/user/9EAEE48CAEE45DF1/cyl_temp/p-bed-data/1e4/"
-#prefix = "../"
+prefix = "../../post/"
 prefix = prefix + "test_pbed_r1_"
 ppy =  131
-R = 10
-num = np.arange(0,100+1)
+R = 8
+num = np.arange(0,200+1)
+Dm = 1.15e-6; 
 
 name = prefix + str(num[0]).zfill(4) + ".h5"
 print(name)
@@ -22,13 +23,15 @@ Ga = np.array(f['Gamma'])
 Nx = int(f['Nx'][0])
 Ny = int(f['Ny'][0])
 ga = Ga.reshape((Ny,Nx))
+ga[np.where(ga>1.0)] = 1.0
 gga = np.sum(ga,axis=1)
 # print(gga)
 print(ppy-1,gga[ppy-1])
 print(ppy,gga[ppy])
 
 
-layer = np.array([ppy-6*R,ppy-4*R,ppy-2*R,ppy])
+layer = np.array([ppy-6*(R+2),ppy-4*(R+2),ppy-2*(R+2),ppy])
+layer = np.array([44,66,88,110,131]);
 M0 = np.zeros((len(layer))) + np.nan
 kkk = []
 for i in range(len(layer)):
@@ -49,14 +52,37 @@ for i in range(len(num)):
 	px = PX[0:-2:3]
 	py = PX[1:-1:3]
 	
-	for ii,c in zip(range(len(layer)),colors):
+	for ii in range(len(layer)):
 		M[i,ii] = float(np.size(np.where(py[kkk[ii]]<=layer[ii])))/M0[ii]
-		plt.plot(np.sqrt(nn),M[:,ii],'*',color=c,label=layer[ii])
-	plt.legend()
 
-	name = 'r' + str(num[i])+'.png'
-	plt.savefig(name,dpi=500)
-	plt.clf()
-	print("generate ",name)
-print M
+k1 = np.zeros(len(layer)) + np.nan
+Vs = np.zeros(len(layer)) + np.nan
+Vt = np.zeros(len(layer)) + np.nan
+Vv = np.zeros(len(layer)) + np.nan
+As = np.zeros(len(layer)) + np.nan
+ 
+for ii,c in zip(range(len(layer)),colors):
+	plt.plot(np.sqrt(nn),M[:,ii],'*',color=c,label=layer[ii])
+	Z = np.polyfit(np.sqrt(nn),M[:,ii],1)
+	k1[ii] = -Z[0]
+	plt.plot(np.sqrt(nn),Z[0]*np.sqrt(nn)+Z[1],color=c)
+	Vs[ii] = np.sum(np.sum(ga[1:layer[ii],:]))
+	Vt[ii] = (layer[ii]*1.0-1.0)*Nx*1.0
+	Vv[ii] = Vt[ii] - Vs[ii]
+	As[ii] = Nx - np.sum(ga[layer[ii],:]) 
+
+
+plt.legend()
+plt.savefig('M.png',dpi=500)
+plt.clf()
+
+Deff = np.pi*(0.5*Vv/As*k1)**2/Dm
+print('Deff/Dm ', Deff)
+plt.plot(Deff,layer,'*')
+plt.ylabel('Depth')
+plt.xlabel('Deff/Dm')
+
+plt.savefig('Deff',dpi=500)
+plt.clf()
+
 f.close()
